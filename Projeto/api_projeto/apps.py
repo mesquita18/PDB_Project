7c6b2aa795ecb5fe1bb43ab1f56025cfa67318e4
AlissonGRN <alissonnascimento.dev@gmail.com>
2026-06-12 11:45:09 -0300

    Cleanup: keep only rest_api

--- Projeto/api_projeto/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiProjetoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api_projeto'
--- Projeto/api_root/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('api_templates.urls')),
-    path('api_templates',include('django.contrib.auth.urls')),
-]
